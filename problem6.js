20e103f2310bd043376cc34a2a412f1c2bb81284
hadialhamza <hamzaglory@gmail.com>
2025-08-04 01:25:44 +0600

    problem 6 solved

---
+++ problem6.js
@@ -0,0 +1,13 @@
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+
+var currentSalary = startingSalary;
+
+for (var i = 1; i <= experience; i++) {
+  currentSalary = currentSalary + (currentSalary * 5) / 100;
+}
+
+var incrementSalary = currentSalary.toFixed(2);
+console.log(incrementSalary);
